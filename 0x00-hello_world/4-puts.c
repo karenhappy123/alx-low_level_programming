db98e1db1b275bcb580aceeae6f0634f685f3d7c
karenhappy123 <karehappy@gmail.com>
2023-06-16 14:36:41 -0700

    tamara

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,10 @@
-#include <stdio.h>
+#include <studio.h>
+/**
+ * main - function for learning
+ * Return: always return 0
+ */
 int main(void)
 {
-  puts("Programming is like building a multilingual puzzle\n";
-  return (0);
-
-}
+  printf("%", "with proper grammar, but the outcome is a piece of art,\n");
+  return (0)
+    }
