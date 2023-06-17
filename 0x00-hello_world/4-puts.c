af07f7060ded4f520cc91d9710e3a542aaf3cfff
karenhappy123 <karehappy@gmail.com>
2023-06-17 06:22:14 -0700

    tamara

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,4 @@
-#include <studio.h>
+#include <stdio.h>
 /**
 * main - function for learning
 * Return: always return 0
@@ -6,5 +6,5 @@
 int main(void)
 {
 printf("%", "with proper grammar, but the outcome is a piece of art,\n");
- return (0);
+return (0);
 }
