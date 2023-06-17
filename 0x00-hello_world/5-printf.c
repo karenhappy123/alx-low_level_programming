36f296a80e62585d5f4a0b887f5642eb7b36bd2f
karenhappy123 <karehappy@gmail.com>
2023-06-17 07:11:18 -0700

    tamara

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+* main - funtion for learning
+* Return: always return 0
+*/
+int main(void)
+{
+printf("%s", "with proper grammar, but the outcome is a piece of art, \n");
+return (0);
+}
