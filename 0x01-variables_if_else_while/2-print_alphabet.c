90609f71fcc5545876218e3becfcdd951d3fb31b
karenhappy123 <karehappy@gmail.com>
2023-06-18 09:10:04 -0700

    karen

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,25 +1,13 @@
-#include <stdlib.h>
-#include <time.h>
-#include <stdio.h>
+#include<stdio.h>
 /**
-* main - function prints check if the random < 5
-* Return: returns 0
+* main - function prints small alphabets
+* Return:  return 0
 */
 int main(void)
 {
-int n;
-int lastDigit;
-srand(time(0));
-n = rand() - RAND_MAX / 2;
-/* your code goes there */
-lastDigit = abs(n) % 10;
-if (n < 0)
-lastDigit *= -1;
-if (lastDigit > 5)
-printf("Last digit of %d is %d and is greater than 5\n", n, lastDigit);
-else if (lastDigit < 6 && lastDigit != 0)
-printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastDigit);
-else
-printf("Last digit of %d is %d and is 0\n", n, lastDigit);
+char c;
+for (c = 'a'; c <= 'z'; c++)
+putchar(c);
+putchar('\n');
 return (0);
 }
