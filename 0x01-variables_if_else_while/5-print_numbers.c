a48c3d92edddb0aa7f2fafaa98704af87d620665
karenhappy123 <karehappy@gmail.com>
2023-06-18 09:37:07 -0700

    karen

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+* main - is the only function
+* Return: always 0
+* author - by abdiwoli hassen
+*/
+int main(void)
+{
+int c;
+for (c = 0; c < 10; c++)
+printf("%d", c);
+}
+putchar('\n');
+return (0);
+}
