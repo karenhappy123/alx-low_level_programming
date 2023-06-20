1d064a4cc23382bd8b4940b4af8ed91a7c91f903
karenhappy123 <karehappy@gmail.com>
2023-06-20 12:28:04 -0700

    mat

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+#include "main.h"
+/**
+* main - main function to cal other functions
+* Return: returns 0
+*/
+int main(void)
+{
+char *text = "_putchar";
+int i;
+for (i = 0; i < 8; i++)
+_putchar(text[i]);
+_putchar('\n');
+return (0);
+}
