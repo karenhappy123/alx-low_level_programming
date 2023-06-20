7c666845a9a38757ae0372d203a8d90469040896
karenhappy123 <karehappy@gmail.com>
2023-06-20 15:18:51 -0700

    mat

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+* _isalpha - function that determines if c is later or number;
+* Return: return 1 or 0
+* @c: argument is an int type
+*/
+int _isalpha(int c)
+{
+return (c >= 65 && c <= 122 ? 1 : 0);
+}
