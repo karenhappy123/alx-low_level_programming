8f79844435a42d512cd694c11ca2f53537c199a2
karenhappy123 <karehappy@gmail.com>
2023-06-20 15:33:51 -0700

    mat

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+* _abs - gets the absolute number
+* Return: absolute int
+* @n: takes an int number
+* author tamara
+*/
+int _abs(int n)
+{
+return (n < 0 ? n * -1 : n > 0 ? n : 0);
+}
