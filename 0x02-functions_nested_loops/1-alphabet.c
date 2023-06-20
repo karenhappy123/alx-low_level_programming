6dea59cd102c5f865fc7cf602c7962b7f3489de4
karenhappy123 <karehappy@gmail.com>
2023-06-20 12:56:45 -0700

    mat

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+* print_alphabet - function prints lower alphpet
+* author tamara
+*/
+void  print_alphabet(void)
+{
+char c;
+for (c = 'a'; c <= 'z'; c++)
+_putchar(c);
+_putchar('\n');
+}
