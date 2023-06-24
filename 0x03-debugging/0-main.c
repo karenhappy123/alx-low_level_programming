edd4ea53788bf7dd705926658908f768f985e9ea
karenhappy123 <karehappy@gmail.com>
2023-06-24 14:01:43 +0300

    done

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,17 +1,16 @@
 #include "main.h"
 
 /**
-* main - function test results
-* Return: Returns 0
+ * main - function test results
+ * Return: Retuns 0
 */
 
 int main(void)
 {
+	int i;
 
-int i;
+	i = 0;
+	positive_or_negative(i);
 
-i = 0;
-positive_or_negative(i);
-
-return (0);
+	return (0);
 }
--- 0x03-debugging/README.md
+++ 0x03-debugging/README.md
@@ -1,1 +1,1 @@
-it
+edugging
